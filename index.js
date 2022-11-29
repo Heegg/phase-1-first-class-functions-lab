6116537873850f0ae897da79005d64f96cd5a5a3
Heeji Hyun <wlgmlgus@gmail.com>
2022-11-29 14:06:47 -0500

    commit tree 205dfd4d03db0a49a8338b7b61771cd926bc27a2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const name = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function(name){
+    return name.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(name){
+    return name.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplierFare) {
+    return function(number) {
+        return multiplierFare * number
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(name, func) {
+    return func(name)
+}
